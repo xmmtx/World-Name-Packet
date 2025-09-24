dd5db20dd4999a365e85418802f70135cbd328d4
枫沐x <98091424+xmmtx@users.noreply.github.com>
2025-09-24 23:07:12 +0800

    Update FabricMod.java

--- src/main/java/pl/kosma/worldnamepacket/FabricMod.java
+++ src/main/java/pl/kosma/worldnamepacket/FabricMod.java
@@ -17,31 +17,26 @@ public class FabricMod implements ModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger();
     
     @Override
-    public void onInitialize() {
-    	ServerPlayNetworking.registerGlobalReceiver(new Identifier(WorldNamePacket.CHANNEL_NAME_VOXELMAP),
-				(server, player, handler, buf, responseSender) -> { sendResponse(player, WorldNamePacket.CHANNEL_NAME_VOXELMAP, buf); });
-    }
+public void onInitialize() {
+    ServerPlayNetworking.registerGlobalReceiver(
+        new Identifier("worldnamepacket", WorldNamePacket.CHANNEL_NAME_VOXELMAP),
+        (server, player, handler, buf, responseSender) -> {
+            sendResponse(player, WorldNamePacket.CHANNEL_NAME_VOXELMAP, buf);
+        }
+    );
+}
 
-    /**
-     * Xaero's Map requires the world name to be send unprompted upon world join/change.
-     */
-    static public void onServerWorldInfo(ServerPlayerEntity player)
-    {
-		sendResponse(player, WorldNamePacket.CHANNEL_NAME_XAEROMAP, null);
-    }
+static private void sendResponse(ServerPlayerEntity player, String channel, @Nullable PacketByteBuf buf) {
+    String levelName = ((MinecraftDedicatedServer) player.getServerWorld().getServer())
+            .getSaveProperties()
+            .getLevelName();
 
-    static private void sendResponse(ServerPlayerEntity player, String channel, @Nullable PacketByteBuf buf)
-    {
-		String levelName = ((MinecraftDedicatedServer) player.getServerWorld().getServer()).getLevelName();
-        byte[] requestBytes = (buf != null) ? buf.slice().array() : new byte[0];
-        byte[] responseBytes = WorldNamePacket.formatResponsePacket(requestBytes, levelName);
+    byte[] requestBytes = (buf != null) ? buf.slice().array() : new byte[0];
+    byte[] responseBytes = WorldNamePacket.formatResponsePacket(requestBytes, levelName);
 
-		PacketByteBuf responsePacket = PacketByteBufs.create();
-		responsePacket.writeBytes(responseBytes);
+    PacketByteBuf responsePacket = PacketByteBufs.create();
+    responsePacket.writeBytes(responseBytes);
 
-        FabricMod.LOGGER.debug("request: " + WorldNamePacket.byteArrayToHexString(requestBytes));
-        FabricMod.LOGGER.debug("response: " + WorldNamePacket.byteArrayToHexString(responseBytes));
-		FabricMod.LOGGER.info("WorldNamePacket: ["+channel+"] sending levelName: " + levelName);
-		ServerPlayNetworking.send(player, new Identifier(channel), responsePacket);
-    }
+    FabricMod.LOGGER.info("WorldNamePacket: [" + channel + "] sending levelName: " + levelName);
+    ServerPlayNetworking.send(player, new Identifier("worldnamepacket", channel), responsePacket);
 }
